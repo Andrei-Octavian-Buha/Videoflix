f875b782b9ce6abf30d9b38a60fde9aa4229adb9
Andrei <buha2595@gmail.com>
2026-07-08 14:11:05 +0200

    feat(auth_app): add template for confirm email/reset password

--- auth_app/tasks.py
+++ auth_app/tasks.py
@@ -6,19 +6,21 @@ from .utils import send_videoflix_mail
 
 @job('default')
 def send_password_reset_mail_task(user_email,uidb64, token):
-    reset_link = f"http://127.0.0.1:8000/api/password_confirm/{uidb64}/{token}/"
 
-    subject ="Reset your password"
-    message = f"You requested a password reset. Please click the link below to set a new password:\n\n{reset_link}"
-    from_email = getattr(settings, 'DEFAULT_FROM_EMAIL', 'noreply@yourdomain.com')
+    context = {
+        'reset_link':f"http://127.0.0.1:8000/api/password_confirm/{uidb64}/{token}/",
+        'from_email' : settings.DEFAULT_FROM_EMAIL,
+    }
 
-    send_mail(subject, message, from_email, [user_email])
+    send_videoflix_mail("Reset your password","password_reset_email",context, user_email)
 
 
 @job('default')
 def send_activation_email_task(user_email, uidb64, token):
+    
     context = {
         'activation_link' : f"http://127.0.0.1:8000/api/activate/{uidb64}/{token}/",
-        'from_email' : settings.DEFAULT_FROM_EMAIL
+        'from_email' : settings.DEFAULT_FROM_EMAIL,
+        'user_name' : user_email
     }
     send_videoflix_mail("Confirm your email","activation_email",context, user_email)
