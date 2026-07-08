fe1201ad38fb5f135dcad3aa0181c08c2f0c0ac0
Andrei <buha2595@gmail.com>
2026-07-08 08:50:30 +0200

    chore(requirements):add pillow

--- video_app/models.py
+++ video_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Video(models.Model):
+    CHATEGORY_COICES = [
+        ('General','General'),
+        ('Drama','Drama'),
+        ('Romance', 'Romance'),
+        ('Action', 'Action'),
+        ('Comedy','Comedy'),
+    ]
+
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    category = models.CharField(max_length=50, choices=CHATEGORY_COICES, default='General')
+    thumbnail = models.ImageField(upload_to='thumbnails/', blank=True, null=True)
+    raw_video_file = models.FileField(upload_to='videos/raw/')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
