ae3ed21062499834f2e884bd630f17d4f6402eaf
Andrei <buha2595@gmail.com>
2026-07-10 12:24:23 +0200

    style(video_app): add optional text to tumbnail models git commit -m

--- video_app/models.py
+++ video_app/models.py
@@ -21,7 +21,7 @@ class Video(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
     category = models.CharField(max_length=50, choices=CHATEGORY_COICES, default='General')
-    thumbnail = models.ImageField(upload_to='thumbnails/',blank=True, null=True)
+    thumbnail = models.ImageField(upload_to='thumbnails/',blank=True, null=True, help_text="Optional: If left blank, a thumbnail will be automatically generated from the video.")
     raw_video_file = models.FileField(upload_to='videos/raw/')
     created_at = models.DateTimeField(auto_now_add=True)
 
